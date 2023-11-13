4965c814c8c774b1a8e00e96292ce72815148c9f
GuiScarabelli <guilherme.quadros@sptech.school>
2023-11-13 18:18:11 -0300

    amem

--- teste-looca - arquivo p teste/src/main/java/App.java
+++ teste-looca - arquivo p teste/src/main/java/App.java
@@ -81,18 +81,19 @@ public class App {
         computador.setQtdDiscos(qtdDicos);
 
         do {
+            ComputadorDAO.cadastrarComputador(computador);
             System.out.println("Digite o usuario: ");
             String emailLogin = entrada.nextLine();
 
             System.out.println("Digite a senha: ");
             String senhaLogin = entrada.nextLine();
 
-            ComputadorDAO.pegarIdComputador(computador);
             UsuarioDAO.pegarUsuario(usuario);
 
             if (!Objects.equals(emailLogin, usuario.getEmail()) || !Objects.equals(senhaLogin, usuario.getSenha())) {
                 System.out.println("usuario ou senha inválidos!");
             } else {
+                ComputadorDAO.pegarIdComputador(computador);
                 System.out.println("Usuário encontrado!");
 
                 System.out.println("""
@@ -127,7 +128,6 @@ public class App {
                         break;
                     case 2:
                         computador.gerarTextoInicio();
-                        ComputadorDAO.pegarIdComputador(computador);
                         StatusPcDAO.pegarIdCaptura(idCaptura);
                         break;
 
